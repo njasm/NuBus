006078fd351c1e86761d0b5f6891e25a74a18323
Nelson J Morais <njmorais@proactive-hosters.com>
2017-01-04 01:23:32 +0000

    re-arrange unit tests

---
+++ NuBusTest/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NuBus;
+using NuBus.Adapter.Extension;
+
+namespace NuBusTest
+{
+	public class BaseTest
+	{
+		public IBus GetBasicBus(string host = "localhost", string user = "guest", string pass = "guest")
+		{
+			var ctg = (new BusConfigurator()).UseRabbitMQ(host,
+				(IBusConfigurator obj) =>
+				{
+					obj.Username(user);
+					obj.Password(pass);
+
+					obj.RegisterAssemblyMessages();
+					obj.RegisterAssemblyHandlers();
+				}
+			);
+
+			return ctg.Build();
+		}
+	}
+}
--- NuBusTest/BusTest.cs
+++ NuBusTest/BusTest.cs
@@ -2,15 +2,76 @@
 using System;
 namespace NuBusTest
 {
+	using System.Threading;
 	using NuBus;
 
 	[TestFixture]
-	public class BusTest
+	public class BusTest : BaseTest
 	{
 		[Test]
 		public void TestArgumentNullExceptionOnBusCreation()
 		{
 			Assert.Throws(typeof(ArgumentNullException),() => new Bus(null)); 
+		}
+
+		[Test]
+		public void TestInstantiation()
+		{
+			var bus = GetBasicBus();
+			Assert.IsInstanceOf(typeof(IBus), bus);
+		}
+
+		[Test]
+		public void TestSendCommand()
+		{
+			var bus = GetBasicBus();
+
+			bus.Start();
+			bus.Send(new Message.PointToPointMessage());
+			bus.Stop();
+		}
+
+		[Test]
+		public void TestSendAsyncCommand()
+		{
+			var bus = GetBasicBus();
+
+			bus.Start();
+			var task = bus.SendAsync(new Message.PointToPointMessage());
+
+			Assert.True(task.Result);
+			bus.Stop();
+		}
+
+		[Test]
+		public void TestPublishEvent()
+		{
+			var bus = GetBasicBus();
+
+			bus.Start();
+			bus.Publish(new Message.PointToPointMessage());
+			bus.Stop();
+		}
+
+		[Test]
+		public void TestPublishAsyncEvent()
+		{
+			var bus = GetBasicBus();
+
+			bus.Start();
+			var task = bus.PublishAsync(new Message.PointToPointMessage());
+
+			Assert.True(task.Result);
+			bus.Stop();
+		}
+
+		[TearDown]
+		public void DrainMessages()
+		{
+			var bus = GetBasicBus();
+			bus.Start();
+			Thread.Sleep(1000);
+			bus.Stop();
 		}
 	}
 }
