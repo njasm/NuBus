0ede1b768b6998757752289eb4912ef6757bf355
Nelson J Morais <njmorais@proactive-hosters.com>
2017-01-31 00:43:03 +0000

    added EndpointConfiguration tests

---
+++ NuBusTest/EndpointConfigurationTest.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using NuBus;
+using NuBusTest.Implementation;
+using System;
+
+namespace NuBusTest
+{
+	[TestFixture]
+	public class EndpointConfigurationTest
+	{
+		public string HostName = "localhost";
+		public SpyBehaviorBusAdapter SpyAdapter { get; set; }
+
+		[SetUp]
+		public void SetUp()
+		{
+			SpyAdapter = new SpyBehaviorBusAdapter();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			SpyAdapter = null;
+		}
+
+		[Test]
+		public void TestThrowArgumentNullException()
+		{
+			Assert.Throws<ArgumentNullException>(() => new EndPointConfiguration(null, SpyAdapter));
+			Assert.Throws<ArgumentNullException>(() => new EndPointConfiguration(HostName, null));
+		}
+
+		[Test]
+		public void TestNotThrow()
+		{
+			var endpoint = new EndPointConfiguration(HostName, SpyAdapter);
+			Assert.IsInstanceOf<IEndPointConfiguration>(endpoint);
+		}
+
+		[TestCase("john")]
+		public void TestUsername(string username)
+		{
+			var endpoint = new EndPointConfiguration(HostName, SpyAdapter);
+			endpoint.Username(username);
+
+			Assert.AreEqual(username, endpoint.GetUsername());
+			Assert.True(SpyAdapter.UsernameCalled);
+		}
+
+		[TestCase("doe")]
+		public void TestPassword(string password)
+		{
+			var endpoint = new EndPointConfiguration(HostName, SpyAdapter);
+			endpoint.Password(password);
+
+			Assert.AreEqual(password, endpoint.GetPassword());
+			Assert.True(SpyAdapter.PasswordCalled);
+		}
+
+		[TestCase("NotLocalHost")]
+		public void TestHostnameIsSet(string host)
+		{
+			var endpoint = new EndPointConfiguration(host, SpyAdapter);
+
+			Assert.AreEqual(host, endpoint.GetHostname());
+		}
+
+		[Test]
+		public void TestAddHandler()
+		{
+			var endpoint = new EndPointConfiguration(HostName, SpyAdapter);
+
+			Assert.Multiple(() => 
+			{
+				Assert.DoesNotThrow(() => endpoint.AddHandler(typeof(Handler.CommandOneHandler)));
+
+				Assert.Throws<ArgumentNullException>(() => endpoint.AddHandler(null));
+				Assert.Throws<InvalidOperationException>(() => endpoint.AddHandler(typeof(IHandler<>)));
+				Assert.Throws<InvalidOperationException>(() => endpoint.AddHandler(this.GetType()));
+			});
+		}
+	}
+}
---
+++ NuBusTest/Implementation/SpyBehaviorBusAdapter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using NuBus;
+using NuBus.Adapter;
+
+namespace NuBusTest.Implementation
+{
+	public class SpyBehaviorBusAdapter : IBusAdapter
+	{
+		public bool UsernameCalled { get; set; }
+		public bool PasswordCalled { get; set; }
+		public bool IsStartedCalled { get; set; }
+		public bool IsPublishCalled { get; set; }
+		public bool IsSendCalled { get; set; }
+		public bool StartCalled { get; set; }
+		public bool StopCalled { get; set; }
+		public bool AcknowledgeMessageCalled { get; set; }
+		public bool AddHandlersCalled { get; set; }
+
+
+		public bool IsStarted
+		{
+			get
+			{
+				this.IsStartedCalled = true;
+				return true;
+			}
+		}
+
+		public event EventHandler<MessageReceivedArgs> HandleMessageReceived;
+
+		public bool AcknowledgeMessage(Guid messageID)
+		{
+			this.AcknowledgeMessageCalled = true;
+			return true;
+		}
+
+		public void AddHandlers(List<Type> handlers)
+		{
+			this.AddHandlersCalled = true;
+		}
+
+		public IBusAdapter Password(string password)
+		{
+			this.PasswordCalled = true;
+			return this;
+		}
+
+		public bool Publish<TEvent>(TEvent EventMessage) where TEvent : IEvent
+		{
+			this.IsPublishCalled = true;
+			return true;
+		}
+
+		public bool Send<TCommand>(TCommand CommandMessage) where TCommand : ICommand
+		{
+			this.IsSendCalled = true;
+			return true;
+		}
+
+		public void Start()
+		{
+			this.StartCalled = true;
+		}
+
+		public void Stop()
+		{
+			this.StopCalled = true;
+		}
+
+		public IBusAdapter Username(string username)
+		{
+			this.UsernameCalled = true;
+			return this;
+		}
+	}
+}
