d309603ae16976c0bad94a53027edab92cee86a2
Nelson J Morais <njmorais@proactive-hosters.com>
2017-02-02 01:48:49 +0000

    XML test class

--- NuBusTest/Extension/XMLTest.cs
+++ NuBusTest/Extension/XMLTest.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using NuBusTest.Message;
+using NUnit.Framework;
+using NuBus.Extension;
+
 namespace NuBusTest.Extension
 {
+	[TestFixture]
 	public class XMLTest
 	{
-		public XMLTest()
+		[Test]
+		public void TestSerializeAndUnserialize()
 		{
+			var subject = new CommandOne();
+			var identifier = subject.ID;
+
+			CommandTwo nullSubject = null;
+
+			Assert.Multiple(() =>
+			{
+				string result = null;
+				Assert.DoesNotThrow(() => result = nullSubject.SerializeToXml());
+				Assert.IsNotNull(result);
+				Assert.IsEmpty(result);
+
+				Assert.DoesNotThrow(() => result = subject.SerializeToXml());
+				Assert.IsNotEmpty(result);
+
+				var resultObj = (CommandOne)result.UnserializeFromXml(subject.GetType().FullName);
+				Assert.IsTrue(identifier.Equals(resultObj.ID));
+
+				result += "malformed</>";
+				Assert.Throws<InvalidOperationException>(() => result.UnserializeFromXml(subject.GetType().FullName));
+			});
 		}
 	}
 }
